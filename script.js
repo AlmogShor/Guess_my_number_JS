47fe119d4e540c537a3cfef94929f389adfd66ee
Almog <shoralmog@gmail.com>
2022-08-18 11:45:25 +0300

    final result. but duplicate code lines

--- script.js
+++ script.js
@@ -8,8 +8,9 @@
 // document.querySelector('.guess').value = 23;
 // document.querySelector('.guess').value;
 
-const ans = Math.trunc(Math.random() * 20) + 1;
+let ans = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = ans;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > ans) {
     // guess is greater than ans
     if (score > 1) {
